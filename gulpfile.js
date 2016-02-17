05220ece6205cf1bfc78c6b56ae34f8fa8748f9f
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2016-02-17 12:10:15 +0700

    set first configuration using npm, bower and gulpjs

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// load all the gulp plugins
+var gulp = require('gulp'),
+	minifycss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
+	stylish = require('gulp-stylish'),
+	uglify = require('gulp-uglify'),
+	usemin = require('gulp-usemin'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	cache = require('gulp-cache'),
+	changed = require('gulp-changed'),
+	rev = require('gulp-rev'),
+	browserSync = require('browser-sync'),
+	del = require('del');	
+	
+// configure all jshint task
+
+// clean
+
+// default
+
+// htmlmin
+
+// usemin
+
+// images
+
+// fonts
+
+// watch 
+
+// browser-sync
