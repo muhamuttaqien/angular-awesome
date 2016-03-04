bb19a484e6f4d4f549881fdbb4f81c365afbd952
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2016-03-04 23:24:58 +0700

    finishing basic configuration for app.js

---
+++ app/scripts/app.controller.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Muh. Angga Muttaqien on 04-Mar-16.
+ */
+
+
+/**
+ * Main controller of the application
+ */
+
+(function(){
+
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AppCtrl', ['$scope', '$rootScope', '$http', '$routeParams', AppCtrl ]);
+
+    function AppCtrl($scope, $rootScope, $http, $routeParams){
+
+    }
+
+})();
