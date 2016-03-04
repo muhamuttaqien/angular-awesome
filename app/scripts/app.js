a52934d379b53bb0e2f2d737baaa80f4e1ba8f74
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2016-03-04 21:53:41 +0700

    adding app module and fixing usemin feature

---
+++ app/scripts/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Muh. Angga Muttaqien on 04-Mar-16.
+ */
+
+
+/**
+ * Main module of the application
+ *
+ * **/
+
+(function(){
+
+    'use strict';
+
+    angular
+        .module('app', [
+
+            // angular core modules
+            'ngRoute',
+            'ngAnimate'
+        ]);
+
+
+})();
