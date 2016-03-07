3bda0877b5ad305c2b8523d71f2a1ec5d1c4c63e
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2016-03-07 13:30:55 +0700

    creating nodeserver is success

--- server.js
+++ server.js
@@ -6,14 +6,36 @@
  * Main server of the applications
  */
 
-var express = require('express');
-var app = express();
+var express         = require('express'),
+    app             = express(),
+    bodyParser      = require('body-parser'),
+    methodOverride  = require('method-override'),
+    mysql           = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    database: "db_jsawesome",
+    user: "root",
+    password: ""
+});
+
 
 app.use('/', express.static(__dirname + ''));
 app.use('/', express.static(__dirname + '/app'));
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/app/index.html');
+});
+
+app.get('/api/mahasiswa', function(req, res){
+    connection.query("SELECT * FROM mahasiswa", function(err, rows, field){
+        res.json(rows);
+    });
 });
 
 app.listen(9090, function(){
